de733192222a75bfea71a54d47edf85ed3294bc2
Rafael Oviedo <rafaeloviedo@Rafaels-Air.fibertel.com.ar>
2024-03-23 11:36:47 -0300

    update: adding loading state when app first loads

--- src/components/GeneralInfo/GeneralInfo.js
+++ src/components/GeneralInfo/GeneralInfo.js
@@ -11,6 +11,7 @@ function GeneralInfo({ onGeneralInfoChange }) {
   const [selectedAlias, setSelectedAlias] = useState('');
   const [isDataStored, setIsDataStored] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [isInfoLoading, setIsInfoLoading] = useState(false);
 
   const handleGeneralInfoChange = (data) => {
     onGeneralInfoChange(data);
@@ -36,6 +37,8 @@ function GeneralInfo({ onGeneralInfoChange }) {
   }
 
   const getCurrentMatch = useCallback(async () => {
+    setIsInfoLoading(true);
+
     const response = await axios.get(`${process.env.REACT_APP_PROD_API}/matches`);
     const currentMatch = response.data[response.data.length - 1];
     setSelectedLocation(currentMatch?.location);
@@ -45,6 +48,8 @@ function GeneralInfo({ onGeneralInfoChange }) {
     if(currentMatch?.location && currentMatch?.date && currentMatch?.alias) {
       setIsDataStored(true);
     }
+
+    setIsInfoLoading(false);
   }, [])
 
   const saveGeneralInfo = async () => {
@@ -81,37 +86,43 @@ function GeneralInfo({ onGeneralInfoChange }) {
       </div>
 
       <div className={style.generalInfoContainer}>
-        <div className={style.inputsContainer} style={ isDataStored ? { display: 'none' } : { height: '100%' }}>
-          <div className={style.inputsRowOneContainer}>
-            <div className={style.locationInputContainer}>
-              <label className={style.locationInputLabel}>Lugar del partido:</label>
-              <select onChange={e => selectLocation(e.target.value)} className={style.locationSelect}>
-                <option value="">Elegi una sede</option>
-                <option value="El Predio - Arregui 2485">El Predio - Arregui 2540</option>
-                <option value="El Poli">El Poli</option>
-                <option value="Futbol Comu">Futbol Comu</option>
-              </select>
-            </div>
+        {
+          isInfoLoading ? (
+            <ProgressSpinner style={{width: '50px', height: '50px', marginTop: '10px'}} strokeWidth="4" />
+          ) : (
+            <div className={style.inputsContainer} style={ isDataStored ? { display: 'none' } : { height: '100%' }}>
+              <div className={style.inputsRowOneContainer}>
+                <div className={style.locationInputContainer}>
+                  <label className={style.locationInputLabel}>Lugar del partido:</label>
+                  <select onChange={e => selectLocation(e.target.value)} className={style.locationSelect}>
+                    <option value="">Elegi una sede</option>
+                    <option value="El Predio - Arregui 2485">El Predio - Arregui 2540</option>
+                    <option value="El Poli">El Poli</option>
+                    <option value="Futbol Comu">Futbol Comu</option>
+                  </select>
+                </div>
 
-            <div className={style.dateInputContainer} disabled>
-              <label className={style.dateInputLabel}>Día del partido:</label>
-              <DatePicker onChange={selectDate} slotProps={{ textField: { size: 'small' } }} />
-            </div>
-          </div>
+                <div className={style.dateInputContainer} disabled>
+                  <label className={style.dateInputLabel}>Día del partido:</label>
+                  <DatePicker onChange={selectDate} slotProps={{ textField: { size: 'small' } }} />
+                </div>
+              </div>
 
-          <div className={style.inputsRowTwoContainer}>
-            <div className={style.paymentAliasInputContainer} disabled>
-              <label className={style.paymentAliasInputLabel}>Alias para el pago:</label>
-              <div className={style.inputAndButtonContainer}>
-                <input onChange={e => handleAliasChange(e.target.value)} onBlur={selectAlias} className={style.paymentAliasInput} value={aliasValue} type='text' />
+              <div className={style.inputsRowTwoContainer}>
+                <div className={style.paymentAliasInputContainer} disabled>
+                  <label className={style.paymentAliasInputLabel}>Alias para el pago:</label>
+                  <div className={style.inputAndButtonContainer}>
+                    <input onChange={e => handleAliasChange(e.target.value)} onBlur={selectAlias} className={style.paymentAliasInput} value={aliasValue} type='text' />
+                  </div>
+                </div>
+              </div>
+
+              <div className={style.saveGeneralInfoButtonContainer}>
+                <button onClick={saveGeneralInfo} className={style.saveGeneralInfoButton} style={!selectedLocation || !selectedDate || !selectedAlias ? { opacity: 0.5, pointerEvents: 'none' } : {}} disabled={!selectedLocation || !selectedDate || !selectedAlias}>{ isLoading ? <ProgressSpinner style={{width: '35px', height: '35px'}} strokeWidth="4" /> : 'Guardar Informacion General'}</button>
               </div>
             </div>
-          </div>
-
-          <div className={style.saveGeneralInfoButtonContainer}>
-            <button onClick={saveGeneralInfo} className={style.saveGeneralInfoButton} style={!selectedLocation || !selectedDate || !selectedAlias ? { opacity: 0.5, pointerEvents: 'none' } : {}} disabled={!selectedLocation || !selectedDate || !selectedAlias}>{ isLoading ? <ProgressSpinner style={{width: '35px', height: '35px'}} strokeWidth="4" /> : 'Guardar Informacion General'}</button>
-          </div>
-        </div>
+          )
+        }
 
         <div className={style.summaryContainer} style={ !isDataStored ? { display: 'none' } : { height: '100%' }}>
           <span><b>Cancha Elegida:</b> { selectedLocation }</span>
